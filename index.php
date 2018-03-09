b234efc697aa90cb4420642dae4447ab35f7280c
bayapucg <vasudevareddy549@gmail.com>
2018-03-09 14:31:27 +0530

    commit
    upoad

--- index.php
+++ index.php
@@ -10,40 +10,40 @@
 								<div class="status-upload">
 									<form>
 										<textarea placeholder="What are you doing right now?" ></textarea>
-
+										
+								<div class="row" id="image_preview"></div>
+								
 										<ul>
 											<li class="image-upload">
-												<div class="image-upload">
-													<label for="file-input">
+													<label for="images">
 														<i class="fa fa-music"></i>
 													</label>
 
-													<input id="file-input" type="file"/>
-												</div>
+													 <input type="file"  id="images" name="images[]" onchange="preview_images();" multiple/>
 											</li>
 											<li class="image-upload">
-												<div class="image-upload">
-													<label for="file-input">
-														<i class="fa fa-video-camera"></i>
+													<label for="images">
+														<i class="fa fa-camera"></i>
 													</label>
-
-													<input id="file-input" type="file"/>
-												</div>
+													 <input type="file"  id="images" name="images[]" onchange="preview_images();" multiple/>
 											</li>
 											<li class="image-upload">
-												<div class="image-upload">
-													<label for="file-input">
-														<i class="fa fa-picture-o " ></i>
+													<label for="images">
+														<i class="fa fa-picture-o"></i>
 													</label>
-
-													<input id="file-input" type="file"/>
-												</div>
+													 <input type="file"  id="images" name="images[]" onchange="preview_images();" multiple/>
 											</li>
+											
 										</ul>
 										<button type="submit" class="btn  btn-primary"><i class="fa fa-share"></i> Post</button>
+									
+								
 									</form>
+									
 								</div><!-- Status Upload  -->
+								
 							</div><!-- Widget Area -->
+							
 						</div>
 		`			</div>
 				
@@ -329,4 +329,90 @@ function myFunction() {
     }
 }
 </script>
+<script>
+function preview_images() 
+{
+ var total_file=document.getElementById("images").files.length;
+ for(var i=0;i<total_file;i++)
+ {
+  $('#image_preview').append("<div class='col-md-2'><img class='img-responsive' src='"+URL.createObjectURL(event.target.files[i])+"'></div>");
+ }
+}
+</script>
+<script>
+ $('#add_more').click(function() {
+          "use strict";
+          $(this).before($("<div/>", {
+            id: 'filediv'
+          }).fadeIn('slow').append(
+            $("<input/>", {
+              name: 'file[]',
+              type: 'file',
+              id: 'file',
+              multiple: 'multiple',
+              accept: 'image/*'
+            })
+          ));
+        });
+
+        $('#upload').click(function(e) {
+          "use strict";
+          e.preventDefault();
+
+          if (window.filesToUpload.length === 0 || typeof window.filesToUpload === "undefined") {
+            alert("No files are selected.");
+            return false;
+          }
+
+          // Now, upload the files below...
+          // https://developer.mozilla.org/en-US/docs/Using_files_from_web_applications#Handling_the_upload_process_for_a_file.2C_asynchronously
+        });
+
+        deletePreview = function (ele, i) {
+          "use strict";
+          try {
+            $(ele).parent().remove();
+            window.filesToUpload.splice(i, 1);
+          } catch (e) {
+            console.log(e.message);
+          }
+        }
+
+        $("#file").on('change', function() {
+          "use strict";
+
+          // create an empty array for the files to reside.
+          window.filesToUpload = [];
+
+          if (this.files.length >= 1) {
+            $("[id^=previewImg]").remove();
+            $.each(this.files, function(i, img) {
+              var reader = new FileReader(),
+                newElement = $("<div id='previewImg" + i + "' class='previewBox'><img /></div>"),
+                deleteBtn = $("<span class='delete' onClick='deletePreview(this, " + i + ")'>X</span>").prependTo(newElement),
+                preview = newElement.find("img");
+
+              reader.onloadend = function() {
+                preview.attr("src", reader.result);
+                preview.attr("alt", img.name);
+              };
+
+              try {
+                window.filesToUpload.push(document.getElementById("file").files[i]);
+              } catch (e) {
+                console.log(e.message);
+              }
+
+              if (img) {
+                reader.readAsDataURL(img);
+              } else {
+                preview.src = "";
+              }
+
+              newElement.appendTo("#filediv");
+            });
+          }
+        });
+</script>
+
 <?php include('footer.php'); ?>
