ef01fbf2d611cbece30b942b0a87d6a8f13ae889
bayapucg <vasudevareddy549@gmail.com>
2018-03-09 15:13:54 +0530

    commit changes

--- application/views/html/admin.php
+++ application/views/html/admin.php
@@ -11,7 +11,13 @@
 									<form>
 										<textarea placeholder="What are you doing right now?" ></textarea>
 										
-								<div class="row" id="image_preview"></div>
+										<div class="row">
+											<div class="col-md-2">
+												<img class="img-responsive" src="https://rukminim1.flixcart.com/image/400/400/iron/f/g/c/philips-gc1905-gc1905-original-imadztntwyhmcmge.jpeg?q=70">
+											</div><div class="col-md-2">
+												<img class="img-responsive" src="https://rukminim1.flixcart.com/image/400/400/iron/f/g/c/philips-gc1905-gc1905-original-imadztntwyhmcmge.jpeg?q=70">
+											</div>
+										</div>
 								
 										<ul>
 											<li class="image-upload">
@@ -329,7 +335,7 @@ function myFunction() {
     }
 }
 </script>
-<script>
+<!--<script>
 function preview_images() 
 {
  var total_file=document.getElementById("images").files.length;
@@ -338,80 +344,6 @@ function preview_images()
   $('#image_preview').append("<div class='col-md-2'><img class='img-responsive' src='"+URL.createObjectURL(event.target.files[i])+"'></div>");
  }
 }
-</script>
-<script>
- $('#add_more').click(function() {
-          "use strict";
-          $(this).before($("<div/>", {
-            id: 'filediv'
-          }).fadeIn('slow').append(
-            $("<input/>", {
-              name: 'file[]',
-              type: 'file',
-              id: 'file',
-              multiple: 'multiple',
-              accept: 'image/*'
-            })
-          ));
-        });
+</script>-->
 
-        $('#upload').click(function(e) {
-          "use strict";
-          e.preventDefault();
-
-          if (window.filesToUpload.length === 0 || typeof window.filesToUpload === "undefined") {
-            alert("No files are selected.");
-            return false;
-          }
-
-          // Now, upload the files below...
-          // https://developer.mozilla.org/en-US/docs/Using_files_from_web_applications#Handling_the_upload_process_for_a_file.2C_asynchronously
-        });
-
-        deletePreview = function (ele, i) {
-          "use strict";
-          try {
-            $(ele).parent().remove();
-            window.filesToUpload.splice(i, 1);
-          } catch (e) {
-            console.log(e.message);
-          }
-        }
-
-        $("#file").on('change', function() {
-          "use strict";
-
-          // create an empty array for the files to reside.
-          window.filesToUpload = [];
-
-          if (this.files.length >= 1) {
-            $("[id^=previewImg]").remove();
-            $.each(this.files, function(i, img) {
-              var reader = new FileReader(),
-                newElement = $("<div id='previewImg" + i + "' class='previewBox'><img /></div>"),
-                deleteBtn = $("<span class='delete' onClick='deletePreview(this, " + i + ")'>X</span>").prependTo(newElement),
-                preview = newElement.find("img");
-
-              reader.onloadend = function() {
-                preview.attr("src", reader.result);
-                preview.attr("alt", img.name);
-              };
-
-              try {
-                window.filesToUpload.push(document.getElementById("file").files[i]);
-              } catch (e) {
-                console.log(e.message);
-              }
-
-              if (img) {
-                reader.readAsDataURL(img);
-              } else {
-                preview.src = "";
-              }
-
-              newElement.appendTo("#filediv");
-            });
-          }
-        });
-</script>
 
