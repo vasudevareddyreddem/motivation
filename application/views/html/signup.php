e3a16acdc10460f19c7c8edd0495d035ca865f5c
vasudevareddyreddem <vasudevareddy549@gmail.com>
2018-03-16 16:44:46 +0530

    adding  name
    adding  name

--- application/views/html/signup.php
+++ application/views/html/signup.php
@@ -5,14 +5,15 @@
             <div class="cell-sm-12  cell-md-6  col-md-offset-3">
               <div class="box">
                 <div class="section-20 inset-left-15 inset-right-15 inset-md-left-30 inset-md-right-30">
-                  <h4>Admin Singup </h4>
+                  <h4>Singup </h4>
                 </div>
                 <hr class="divider offset-none">
                 <div class="section-30 inset-left-15 inset-right-15 inset-md-left-30 inset-md-right-30">
                  
                
                   <form  method="POST" action="<?php echo base_url('admin/singuppostpost'); ?>">
-                    <input class="form-control" type="text" id="email" name="email"  placeholder="Email Adress" required><br> 
+                    <input class="form-control" type="text" id="name" name="name"  placeholder="Name" required><br> 
+                    <input class="form-control" type="email" id="email" name="email"  placeholder="Email Adress" required><br> 
                     <input class="form-control" type="text" id="mobile" name="mobile"  placeholder="Enter Mobile" required><br> 
 					<input class="form-control" type="password" id="password" name="password" placeholder="Enter Password " required><br> 
 					<input class="form-control" type="password" id="confirmpassword" name="confirmpassword" placeholder="Enter Confirm Password " required><br> 
