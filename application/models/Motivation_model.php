35de4a877c1645e563c791cd203d43b7da6df063
vasudevareddyreddem <vasudevareddy549@gmail.com>
2018-03-23 17:41:22 +0530

    changees purose

--- application/models/Motivation_model.php
+++ application/models/Motivation_model.php
@@ -224,6 +224,7 @@ class Motivation_model extends CI_Model
 		$this->db->select('FROM_BASE64(post_count.p_id) ,TO_BASE64(post_count.p_id) as url,post_count.p_id,post_count.title,LEFT(post_count.text, 40) as lit,post_count.create_at,post_count.image_count')->from('post_count');		
 		$this->db->like('title',$search);
 		$this->db->or_like('text', $search);
+		$this->db->limit(6);
 		return $this->db->get()->result_array();
 	}
 	public function update_like_count($data){
